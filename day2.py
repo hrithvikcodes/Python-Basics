bfe7f41270f0a059846f14728fb37488960b5b9e
hrithvikcodes <hrithvikmypali@gmail.com>
2025-06-27 21:55:11 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,9 @@
+#  Checking Voting Eligibility 
+print("Welcome!!")
+name = input("What's your name ?") 
+print("Hello, " +name+"!")
+
+age= int(input("Enter your age:"))
+
+if age>=18 : print("Eligible to vote")
+else : print("You are not eligible to vote") 
